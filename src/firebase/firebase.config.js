b98a820dd67fee36a3c9782422012d7cd0965ba7
Mustafizur Rahman <mustafizur232@gmail.com>
2023-12-29 12:19:00 +0600

    project set up with dasiy ui and tailwind css. Also different route defined

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcV2CZGg6NgV8Ho9wH1F4pfqe1c6Dt5nQ",
+  authDomain: "auth-combination-69077.firebaseapp.com",
+  projectId: "auth-combination-69077",
+  storageBucket: "auth-combination-69077.appspot.com",
+  messagingSenderId: "782633550254",
+  appId: "1:782633550254:web:f3deb31d98a4778792c83f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
